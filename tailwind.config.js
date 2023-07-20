1c807d40ae4b89e2ff9fa6d2fe26e77552cd647d
udohchuks <urichard132@gmail.com>
2023-07-20 18:32:32 +0100

    new version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./build/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1000px',
+      xl:'1440px'
+    },
     extend: {
-      fontFamily: {
-        'vietnam': ['Be Vietnam Pro', 'sans-serif']
-      },
       colors: {
-        'primary-bright-red': 'hsl(12, 88%, 59%)',
-        'primary-dark-blue': 'hsl(228, 39%, 23%)',
-        'secondary-gray-blue': 'hsl(227, 12%, 61%)',
-        'secondary-very-dark-blue': ' hsl(233, 12%, 13%)',
-        'secondary-pale-red': 'hsl(13, 100%, 96%)',
-        'secondary-light-grey': 'hsl(0, 0%, 98%)',
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 13%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+
       },
-      backgroundImage: {
-        'illustration-mobile': "url('images/bg-simplify-section-mobile.svg')",
-        'illustration-tablet': "url('images/bg-tablet-pattern.svg')",
-        'illustration-desktop': "url('images/bg-simplify-section-desktop.svg')",
+      gridTemplateColumns: {
+        'auto':'repeat(auto-fit, minmax(300px, 1fr))'
       }
+    },
+    container: {
+      center: true,
     },
   },
   plugins: [],
